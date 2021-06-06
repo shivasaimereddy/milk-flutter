e415250922f75cdc526980c26126621582b364c1
VenkatRamanaReddyMarri <marrivenkatramanar@gmail.com>
2021-06-06 12:09:23 +0530

    Added Home Screen as Initial Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:milk_delivery_flutter/components/coustom_bottom_nav_bar.dart';
+import 'package:milk_delivery_flutter/enums.dart';
+import 'package:milk_delivery_flutter/screens/home/drawer_screen.dart';
+import 'package:milk_delivery_flutter/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Milk Delivery App'),
-        ),
-        body: Container(
-          child: Center(
-            child: Text('Hello, Lets build our Project!')
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
   }
 }
