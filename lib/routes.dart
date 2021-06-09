f239da3c8aa8f5c6a0b66e6183d6f143d2202512
shivasaimereddy <shivasaimereddy@gmail.com>
2021-06-09 20:17:42 +0530

    Added Splash Screen Route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:milk_delivery_flutter/screens/home/home_screen.dart';
+import 'package:milk_delivery_flutter/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   HomeScreen.routeName: (context) => HomeScreen(),
+  SplashScreen.routeName: (context) => SplashScreen(),
 };
