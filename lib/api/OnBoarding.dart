c50652a59e86b900d6e112ff79000a3cf8eb21cd
shivasaimereddy <shivasaimereddy@gmail.com>
2021-06-09 20:43:17 +0530

    Created functionality to get On Boarding Data from API

---
+++ lib/api/OnBoarding.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:milk_delivery_flutter/models/OnBoarding.dart';
+import 'package:milk_delivery_flutter/server.dart';
+
+Future<List<OnBoarding>> fetchScreens() async {
+  final response =
+      await http.get(Uri.parse('$server/api/constants/onboarding/'));
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => new OnBoarding.fromJson(data)).toList();
+  } else {
+    throw Exception('Unexpected error occured!');
+  }
+}
