5f5250b75921cafc7f53a2eab9fc6a6d1389caa5
shivasaimereddy <shivasaimereddy@gmail.com>
2021-06-09 20:15:07 +0530

    Connected Base API to server file

--- lib/api/Carousel.dart
+++ lib/api/Carousel.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:milk_delivery_flutter/models/Carousel.dart';
+import 'package:milk_delivery_flutter/server.dart';
 
 Future<List<Carousel>> fetchCarousel() async {
-  final response =
-      await http.get(Uri.parse('http://10.0.2.2:8000/api/carousel/'));
+  final response = await http.get(Uri.parse('$server/api/carousel/'));
   if (response.statusCode == 200) {
     List jsonResponse = json.decode(response.body);
     return jsonResponse.map((data) => new Carousel.fromJson(data)).toList();
