f1eddc8e1b4ab4c88d9df64c2e3abe75f0e4f282
shivasaimereddy <shivasaimereddy@gmail.com>
2021-06-27 13:38:22 +0530

    Route for product details screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:milk_delivery_flutter/screens/home/home_screen.dart';
+import 'package:milk_delivery_flutter/screens/productDetails/product_details_screen.dart';
 import 'package:milk_delivery_flutter/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   HomeScreen.routeName: (context) => HomeScreen(),
   SplashScreen.routeName: (context) => SplashScreen(),
+  ProductDetailsScreen.routeName: (context) => ProductDetailsScreen(),
 };
